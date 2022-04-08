877715bc04f97f9f6eaa5bca88a10d370890e214
jinghongdaxiong <18032169350@163.com>
2022-04-08 15:20:25 +0800

    ADD

---
+++ README.md
@@ -0,0 +1,12 @@
+# 探索系统设计
+
+本书翻译自Grokking + System + Design
+
+## 作者信息
+
+* [博客](https://iwantjingjing.com/)
+* [github](https://github.com/jinghongdaxiong)
+* 微信
+
+![img_6.png](img_6.png)
+
---
+++ _book/mkdir.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+touch 一致性哈希.md 长轮询与WebSockets与服务器发送事件.md 系统设计演进指南.md 设计像TinyURL这样的短URL服务.md 设计Instagram.md 设计Dropbox.md 设计FackbookMessager.md 设计Twitter.md 设计Youtube或Netflix.md 设计Twitter搜索.md 设计一个Web爬虫.md 设计Facebook的新闻流.md 设计Yelp或附近的朋友.md 设计Uber后端.md 设计BookMyShow.md
